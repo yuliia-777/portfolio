1be7c701257574ef56acdfc8a07bfeb0c0797630
Yuliia Aleksiienko <yuliia.aleksiienko@gmail.com>
2024-07-20 00:18:37 +0300

    add avatar

--- assets/cart-drawer.js
+++ assets/cart-drawer.js
@@ -4,7 +4,7 @@ class CartDrawer extends HTMLElement {
 
     this.addEventListener('keyup', (evt) => evt.code === 'Escape' && this.close());
     this.querySelector('#CartDrawer-Overlay').addEventListener('click', this.close.bind(this));
-    this.setHeaderCartIconAccessibility();
+    // this.setHeaderCartIconAccessibility();
   }
 
   setHeaderCartIconAccessibility() {
